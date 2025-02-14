bd3e87889971fcedce35fd41554e4758f135ccb5
Luciano Albani <luchoalbanix1@gmail.com>
2025-02-14 20:21:22 -0300

    invitacion

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var muteButton = document.getElementById('muteButton');
+
+    muteButton.addEventListener("click", function() {
+        alert("MIRA QUE TE VOY A DEJAR MUTEAR ESTE TEMAZO AGUANTE DOMO ARIGATO MR ROBOTO...🤖");
+    });
+});
